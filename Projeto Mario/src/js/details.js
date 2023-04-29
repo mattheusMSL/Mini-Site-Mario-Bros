b70cbac27ea5ea0ba0a8392acf2718768ee54935
Matheus Santos lopes <55161799+mattheusMSL@users.noreply.github.com>
2023-04-29 12:53:29 -0300

    update de scroll desc mario

--- Projeto Mario/src/js/details.js
+++ Projeto Mario/src/js/details.js
@@ -1,23 +1,42 @@
 
-const target = document.querySelectorAll('.img-mario');
+const imgMario = document.querySelectorAll('.img-mario');
  
-window.addEventListener('scroll', animaScroll);
+window.addEventListener('scroll', animaScrollMario);
 
-function animaScroll(){
+function animaScrollMario(){
  const trigger = window.innerHeight / 5 * 5;
 
-target.forEach(element => {
-  const targetScroll = element.getBoundingClientRect();
-  
-  if (targetScroll.top < trigger){
+ imgMario.forEach(element => {
+ const imgMarioScroll = element.getBoundingClientRect();
+  if (imgMarioScroll.top < trigger){
     element.classList.add('animated-mario')
   } else {
     element.classList.remove('animated-mario')
   }
-
 });
+}
+
+/*  ------------ imagem do mario ------------  */ 
+
+
+const descMario = document.querySelectorAll('.animated-mario-desc');
+
+window.addEventListener('scroll', animaScrollDescMario);
+
+function animaScrollDescMario(){
+  const trigger = window.innerHeight / 5 * 5;
+
+  descMario.forEach((element) =>{
+    const descMarioScroll = element.getBoundingClientRect();
+    if (descMarioScroll.top < trigger){
+      element.classList.add('animated-mario-desc')
+    } else {
+      element.classList.remove('animated-mario-desc')
+    }
+  });
 
 }
+
 
 
 
