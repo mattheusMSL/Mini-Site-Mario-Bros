d466d976fe2c15ba85a8637bb0819169741fea93
Matheus Santos lopes <55161799+mattheusMSL@users.noreply.github.com>
2023-04-29 04:38:24 -0300

    scroll attempt

--- Projeto Mario/src/js/script.js
+++ Projeto Mario/src/js/script.js
@@ -23,4 +23,36 @@ botaoTrailer.addEventListener("click", () => {
  });
  
 
- const target = document.querySelector(".animated-mario");
+ const target = document.querySelector(".img-mario");
+ const animationClass = '.animated-mario';
+
+ function animeScroll(){
+	 const windowTop = window.innerHeight * 0.75;
+	 target.forEach(element => {
+		if((windowTop) > element.offsetTop){
+			element.classList.add(animationClass);
+		}else{
+			element.classList.remove(animationClass);
+		}
+	 });
+ }
+
+ animeScroll();
+
+ if(target.length){
+	window.addEventListener('scroll',() =>{
+		animeScroll();
+	} )
+ }
+
+// const observer = new IntersectionObserver(entries => {
+// 	entries.forEach(entry => {
+// 		if(entry.isIntersecting){
+// 			document.querySelector(".animated-mario")[0].classList.add("")
+// 		}
+// 	})
+
+// })
+
+
+// observer.observe(document.querySelector(".container"))
